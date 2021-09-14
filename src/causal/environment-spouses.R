e33f08127d181e5b3f3a962528ba66f0044d8058
Sebastian Daza <sebastian.daza@gmail.com>
2021-09-14 11:59:36 +0200

    update ABM

--- src/causal/environment-spouses.R
+++ src/causal/environment-spouses.R
@@ -5,27 +5,65 @@
 
 library(data.table)
 library(brms)
-library(performance)
+library(texreg)
 
-dat = fread("models/BMI-spouses-GxE/output/bmi_0_0.csv")
+path = "models/BMI-spouses-GxE/output/"
+files = paste0(path, list.files(path, pattern = "bmi"))
+dat = rbindlist(lapply(files, fread))
 
+files = paste0(path, list.files(path, pattern = "parameter"))
+params = rbindlist(lapply(files, fread))
 
-summary(dat$bmi)
-summary(dat$bmi_spouse)
+table(dat$iteration)
+table(dat$replicate)
+
+cor(dat$bmi, dat$bmi_spouse)
+
+dat
+params
+
+m0 = brm(bmi ~ pgs + environment + pgs * environment, 
+    data = dat, iter = 2000, chains = 1, backend = "cmdstan")
+summary(m0)
+
+m1 = brm(bmi ~ pgs + environment + pgs * environment + unobserved +
+    pgs * unobserved, 
+    data = dat, iter = 2000, chains = 1, backend = "cmdstan")
+summary(m1)
+
+m2 = brm(bmi ~ pgs + bmi_spouse + pgs * bmi_spouse, 
+    data = dat, iter = 2000, chains = 1, backend = "cmdstan")
+summary(m2)
+ 
+
+screenreg(list(m0, m1))
+params
 
 # hist(dat$bmi)
 # hist(dat$bmi_spouse)
 # hist(dat$pgs)
 # hist(dat$pgs_spouse)
 
+models = list()
+for (i in 1:100) {
+    temp = dat[iteration == 1 & replicate == i]
+    models[[i]] =  brm(bmi ~ pgs + environment + pgs * environment, 
+    data = temp, iter = 2000, chains = 1, backend = "cmdstan")
+
+}
+m1 = combine_models(mlist = models, check_data = FALSE)
+summary(m1)
+
+
 cor(dat$bmi, dat$bmi_spouse)
 cor(dat$pgs, dat$pgs_spouse)
 cor(dat$environment, dat$environment_spouse)
 
+
 dat[, couple := paste0(sort(.SD), collapse = ""), .SDcols = c("id", "spouse_id"), 
     by = 1:nrow(dat)]
 setorder(dat, couple)
-
+dat
 
 m0 = brm(bmi ~ pgs + environment + pgs * environment, 
     data = dat, iter = 2000, chains = 1)
@@ -36,4 +74,4 @@ m1 = brm(bmi ~ pgs + bmi_spouse + pgs * bmi_spouse,
 summary(m1)
 
 
-icc(m1, by_group = FALSE)
+
